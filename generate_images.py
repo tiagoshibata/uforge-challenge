0452055b1a91c093632f40d53f58fbebf94049ef
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2021-10-16 17:38:40 -0700

    Pairwise alignment for all pairs

---
+++ generate_images.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import subprocess
+
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
+import sam
+
+print('Loading FASTA...')
+ref = Path(__file__).parent / 'S288C_reference_genome_R64-3-1_20210421/S288C_reference_sequence_R64-3-1_20210421.fsa'
+chromossomes = [r.id for r in SeqIO.parse(ref, 'fasta')]
+
+for comp in (Path(__file__).parent / 'diff').glob('*.sam'):
+    print(comp)
+    data = sam.SamReader(comp)
--- pairwise_alignement.py
+++ pairwise_alignement.py
@@ -11,18 +11,22 @@ def fasta_to_dict(path):
 
 print('Loading FASTA...')
 ref = fasta_to_dict(Path(__file__).parent / 'S288C_reference_genome_R64-3-1_20210421/S288C_reference_sequence_R64-3-1_20210421.fsa')
-variants = {'ref': ref}
+# variants = {'ref': ref}
+variants = {}
 for x in (Path(__file__).parent / 'aligned_to_reference').glob('*.fa'):
     if 'ref' in x.name:
         continue
     seqs = fasta_to_dict(x)
     print(f'{x} -> {list(seqs.keys())}')
-    variants[x.name[:-4]] = seqs
+    variants[x.name[:-3]] = seqs
 
-# For each chromossome, run multiple sequence alignement
-for chromossome in ref:
-    print(f'Multiple Sequence Alignment on {chromossome}')
-    records = [SeqRecord(seqs[chromossome].seq, id=variant) for variant, seqs in variants.items()]
-    chromossome_file = Path(__file__).parent / f'aligned_to_reference/{chromossome}.fa'
-    SeqIO.write(records, chromossome_file, 'fasta')
-    subprocess.run(['muscle', '-in', chromossome_file, '-out', f'{chromossome_file}_aligned.fasta', '-maxiters', '1', '-diags1', '-sv'], check=True)
+# For each variant pair, run pairwise alignement
+for v1 in variants:
+    for v2 in variants:
+        if v2 <= v1:
+            continue
+        # for chromossome in ref:
+        print(f'Aligning {v1} x {v2}')
+        with open(f'diff/{v1}_x_{v2}.sam', 'w') as f:
+            subprocess.run(['minimap2', '-a', Path(__file__).parent / f'aligned_to_reference/{v1}.fa', Path(__file__).parent / f'aligned_to_reference/{v2}.fa'],
+                    check=True, stdout=f)
